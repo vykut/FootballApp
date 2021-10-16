54770f8d3c1d5d7788507b8281415c3c0f823bbf
Victor Socaciu <vsocaciu@aligntech.com>
2021-10-16 12:18:10 +0300

    Initial Commit

---
+++ FootballApp/FootballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAppApp.swift
+//  FootballApp
+//
+//  Created by vsocaciu on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FootballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
